a4bdb135817486e9a7948a5010808678f8d19eb4
Clinton Lunn <cpl09c@gmail.com>
2020-02-09 17:06:58 -0500

    get cards into grif, add css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
 import { render } from "@testing-library/react";
+import { CardList } from "./components/card-list/card-list";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "abc"
-        },
-        {
-          name: "Dracula",
-          id: "def"
-        },
-        {
-          name: "Zombie",
-          id: "hij"
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name="Clinton">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
